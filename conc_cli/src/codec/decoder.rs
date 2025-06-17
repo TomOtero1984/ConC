4e0a06a5a2c97a9ef6369c57e789ebd9d617c8dc
tomotero1984 <tomotero1984@gmail.com>
2025-06-17 16:40:26 -0400

    feature: add packet scheme docs

--- conc_cli/src/codec/decoder.rs
+++ conc_cli/src/codec/decoder.rs
@@ -1,22 +1,24 @@
+// src/decoder.rs
+
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
-use crate::symbol_map::SymbolMap;
+use crate::charset::SymbolMap;
 
-pub fn encode_file(input_path: String, output_path: String, map: &SymbolMap) {
+pub fn decode_file(input_path: String, output_path: String, map: &SymbolMap) {
     let input_file = File::open(&input_path).expect("Failed to open input file");
     let reader = BufReader::new(input_file);
     let mut output_file = File::create(&output_path).expect("Failed to create output file");
 
     for line in reader.lines() {
         let line = line.expect("Failed to read line");
-        let encoded_line = encode_line(&line, map);
-        writeln!(output_file, "{}", encoded_line).expect("Failed to write line");
+        let decoded_line = decode_line(&line, map);
+        writeln!(output_file, "{}", decoded_line).expect("Failed to write line");
     }
 }
 
-fn encode_line(line: &str, map: &SymbolMap) -> String {
+fn decode_line(line: &str, map: &SymbolMap) -> String {
     line.split_whitespace()
-        .map(|word| map.lookup(word).unwrap_or(word).to_string())
+        .map(|sym| map.reverse_lookup(sym).unwrap_or(sym).to_string())
         .collect::<Vec<String>>()
         .join(" ")
 }
