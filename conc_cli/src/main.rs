f9ed183283b07540c0ab6960501589f8060bf4f6
Tom Otero <52332614+TomOtero1984@users.noreply.github.com>
2025-06-24 17:21:35 -0400

    Feature/generate word map (#1)
    * task: create lib.rs, update main to use lib.rs, update .gitignore
    
    * feature: add support for conc_cli to generate word maps from newline delimited word lists
    
    * task: cleaning obsolete files

--- conc_cli/src/main.rs
+++ conc_cli/src/main.rs
@@ -1,14 +1,7 @@
-use clap::{Parser, Subcommand};
 use std::fs;
-
-mod codec;
-use codec::encoder;
-use codec::decoder;
-mod symbols;
-use symbols::charset::SYMBOL_CHARSET;
-use symbols::map::SymbolMap;
-
-
+use std::process::Output;
+use clap::{Parser, Subcommand};
+use conc_cli::{encoder, decoder, SymbolMap, symbols};
 #[derive(Parser)]
 #[command(
     name = "conc",
@@ -40,6 +33,17 @@ enum Commands {
 
         #[arg(short = 'm', long)]
         map_dir: String,
+    },
+    GenerateWordMap {
+        #[arg(short, long)]
+        input: String,
+        
+        #[arg(short, long)]
+        output: Option<String>,
+    },
+    ValidateJsonl {
+        #[arg(short, long)]
+        input: String,
     },
 }
 
@@ -85,6 +89,15 @@ fn main() {
             let map_paths = files.iter().map(|s| s.as_str()).collect::<Vec<_>>();
             let map = SymbolMap::from_jsonl_files(&map_paths);
             decoder::decode_file(input, output, &map);
+        }
+        Commands::GenerateWordMap { input, output  } => {
+            println!("Generating ConC word map from: {}", input);
+            println!("Saving to: {}", output.as_deref().unwrap_or("None (stdout)"));
+            symbols::utils::generate_word_map(&input, output.as_deref());
+        }
+        Commands::ValidateJsonl { input } => {
+            println!("Validating: {}", input);
+            symbols::utils::validate_jsonl(&input);
         }
     }
 }
